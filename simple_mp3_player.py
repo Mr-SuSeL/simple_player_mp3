e352dffc535bfff62b88c0a14cf229c3cbeb0137
Lukasz Suslowicz <susel.invest@gmail.com>
2024-02-29 18:57:29 +0100

    raw version of app works fine

--- simple_mp3_player.py
+++ simple_mp3_player.py
@@ -10,16 +10,17 @@ pygame.mixer.init()
 
 
 def play():
-    pass
+    pygame.mixer.music.load('mp3.mp3')
+    pygame.mixer.music.play(loops=0)
 
 def stop():
-    pass
+    pygame.mixer.music.stop()
 
 
-play_button = Button()
-play_button.pack()
+play_button = Button(root, text='PLAY', command=play)
+play_button.pack(pady=20)
 
-stop_button = Button()
-stop_button.pack()
+stop_button = Button(root, text='STOP', command=stop)
+stop_button.pack(pady=20)
 
 root.mainloop()
