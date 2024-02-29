ca541e750d9515a07877133c386ac99bf3a0f96b
Lukasz Suslowicz <susel.invest@gmail.com>
2024-02-29 18:38:12 +0100

    gui and library initialized

---
+++ simple_mp3_player.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import pygame
+
+root = Tk()
+root.title('music player')
+root.iconbitmap('music-icon.jpg')
+
+pygame.mixer.init()
+
+
+def play():
+    pass
+
+def stop():
+    pass
+
